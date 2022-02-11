64ee957f221f30c5515dfae1714975d5ea2387e6
yash-patel268 <yashp2126@gmail.com>
2022-02-11 16:51:08 -0500

    Added card class

---
+++ src/com/company/Card.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Card {
+    private String suit;
+    private String value;
+
+    public String getSuit(){
+        return suit;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+    public Card(String value,String suit){
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public void setCard(String value,String suit){
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public String toString(){
+        String name = value + " of " + suit;
+        return name;
+    }
+}
